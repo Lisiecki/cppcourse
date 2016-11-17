67145eff8fd804e63f0c2264b98614ec79609bb7
Lisiecki <dennislisiecki@gmail.com>
2016-11-17 12:09:56 +0100

    finished ConvertBinary functionality

--- ConvertBinary.cpp
+++ ConvertBinary.cpp
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include <iostream>;
+#include <math.h>
+#include <iostream>
 
 using namespace std;
 
 int num_digits(const int numb) {
 	int i = 1;
 	for (;; i++) {
-		if ((numb < pow(2, i)) {
+		if (numb < pow(2, i)) {
 			break;
 		}
 	}
@@ -15,12 +15,29 @@ int num_digits(const int numb) {
 }
 
 void decimal2binary(const int a, int d[], const int n) {
-	for (int i = n - 1; i >= 0; i++) {
-		d[i] = a % 2;		
-		a /= 2;
+	int tmp = a;
+	for (int i = n - 1; i >= 0; i--) {
+		d[i] = tmp % 2;		
+		tmp /= 2;
 	}
 }
 
 int main() {
+	int dec_numb, bin_length;
+	int *bin_numb;
 
+	cout << "Please enter a number > 0: ";
+	cin >> dec_numb;
+
+	bin_length = num_digits(dec_numb);
+	bin_numb = new int[bin_length];
+	decimal2binary(dec_numb, bin_numb, bin_length);
+	cout << "Decimal: " << dec_numb << " Binary: ";
+	for (int i = 0; i < bin_length; i++) {
+		cout << bin_numb[i];
+	}
+	cout << endl;
+
+	delete[] bin_numb;
+	return 0;
 }
