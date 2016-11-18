ebe2c1f9cb9fb55aed5ad11ccca0b8f569361e03
Lisiecki <dennislisiecki@gmail.com>
2016-11-18 15:54:23 +0100

    added comments and optimized calculation of amount of binary digits for exercise 4.4

--- ConvertBinary.cpp
+++ ConvertBinary.cpp
@@ -4,9 +4,12 @@
 using namespace std;
 
 int num_digits(const int numb) {
-	int i = 1;
+	int i = 1, tmp = 1;
+	// bitshift 2 i times to the left until numb is smaller than 2^i
+	// i is then the amount of binary digits we need to represent numb as a binary number
 	for (;; i++) {
-		if (numb < pow(2, i)) {
+		tmp = tmp << 1;
+		if (numb < tmp) {
 			break;
 		}
 	}
@@ -16,6 +19,10 @@ int num_digits(const int numb) {
 
 void decimal2binary(const int a, int d[], const int n) {
 	int tmp = a;
+	// determine the binary digits that represents "a" from left to right
+	// calculate (tmp modulo 2) to determine one binary digit, then divide tmp by 2
+	// to calculate the next binary digit until each binary digit that represents "a"
+	// is calculated
 	for (int i = n - 1; i >= 0; i--) {
 		d[i] = tmp % 2;		
 		tmp /= 2;
@@ -29,15 +36,19 @@ int main() {
 	cout << "Please enter a number > 0: ";
 	cin >> dec_numb;
 
+	// determine the binary representation of a decimal number
 	bin_length = num_digits(dec_numb);
 	bin_numb = new int[bin_length];
 	decimal2binary(dec_numb, bin_numb, bin_length);
+
+	// print the result
 	cout << "Decimal: " << dec_numb << " Binary: ";
 	for (int i = 0; i < bin_length; i++) {
 		cout << bin_numb[i];
 	}
 	cout << endl;
 
+	// release allocated memory
 	delete[] bin_numb;
 	return 0;
 }
