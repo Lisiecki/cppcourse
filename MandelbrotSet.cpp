354f0913ebc69a8021450dabe25f812c414e1663
Lisiecki <dennislisiecki@gmail.com>
2016-12-02 21:01:30 +0100

    ex 2 (needs to be reviewed)

---
+++ MandelbrotSet.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <complex>
+#include <fstream>
+
+#define N 350
+#define M 200
+#define N_MAX 1000
+
+// Mandelbrot set is the set of complex numbers z_m for which the sequence z_n+1 = z^2_n + z_m with z_0 = 0 does not approach infinity
+
+double calc_x_n(int n) {
+	static double it_x = 3.5;
+	return -2.5 + it_x * n;
+}
+
+double calc_y_m(int m) {
+	double it_y = 2.0;
+	return -1 + it_y * m;
+}
+
+// generate complex number z_m = x_n + y_m * i
+std::complex<double> generate_z_m(double x_n, double y_m) {
+	std::complex<double> z_m(x_n, y_m);
+	return z_m;
+}
+
+double calculate_c(const double n_max, double n) {
+	return (n_max - n) / n_max;
+}
+
+int main() {
+	std::ofstream outfile;
+
+	//table.dat als outfile öffnen
+	outfile.open("mandelbrot.dat");
+	// generate a NxM-grid of points (x_m, y_m) with -2.5 <= x_m <= 1 and -1 <= y_m <= 1, i.e. for each of the N points along the x-axis compute M points along the y-axis in the given ranges
+	for (int n = 0; n < N; n++) {
+		for (int m = 0; m < M; m++) {		
+			// use z_m = x_m + y_m * i and starting with z_0 = 0 iterate until either |z_n| >= 2 or z_n_max is reached
+			std::complex<double> z_m = generate_z_m(calc_x_n(n), calc_y_m(m));
+			for (double z_n = 0.0, i = 0.0; z_n < 2.0 && i < N_MAX; i++) {
+				z_n = z_n * z_n + z_m.real();
+				// for every point (x_n, y_m) write to a file named mandelbrot.dat one line with the coordinates of the point and the value c = (n_max - n)/n_max
+				outfile << "(" << n << ", " << m << ")\t" << calculate_c(N_MAX, i) << std::endl;
+			}
+			
+		}
+	}
+
+	outfile.close();
+
+	return 0;
+}
