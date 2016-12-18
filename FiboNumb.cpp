1e56d70fe352740e3d1a928d613660b935c877a2
Lisiecki <dennislisiecki@gmail.com>
2016-12-18 17:00:39 +0100

    FiboNumb

---
+++ FiboNumb.cpp
@@ -0,0 +1,26 @@
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include "Fibonacci.h"
+
+using namespace std;
+
+int main() {
+	int n = 50;
+	unsigned long int f_n;
+	std::ofstream outfile;
+	outfile.open("fibonacci.dat");
+
+	clock_t cpu_time, curr_cpu_time;
+	Fibonacci fibo;
+	
+	for (int i = 1; i <= n; i++) {
+		cpu_time = clock();
+		f_n = fibo(i);
+		curr_cpu_time = clock() - cpu_time;
+		outfile << f_n << "\t" << i << "\t" << double(curr_cpu_time)/CLOCKS_PER_SEC << "\t" << endl;
+	}
+
+	outfile.close();
+	return 0;
+}
